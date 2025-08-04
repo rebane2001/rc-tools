6a91f4ac9857c2eb8c8123bff85f6d100b5e8e7a
rebane2001 <rebane2001@gmail.com>
2025-08-04 22:00:33 +0300

    flask example

---
+++ Generic/flask_basic.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+rc-tools - flask basic example
+==============================
+
+note: flask automatically serves files in a ./static/ folder
+
+Get data from a (usually) POST request:
+request.data - as string
+request.args - url params
+request.form
+request.files
+request.values - args + form
+request.json
+
+note: use request.get_json(force=True) to ignore mime
+
+"""
+from flask import Flask, send_file, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return f"index"
+
+# http://127.0.0.1:8000/hi?name=lyra
+@app.route('/hi')
+def hi():
+    return f"hi, {request.args.get('name')}!"
+
+@app.route('/lyra.png')
+def file():
+    return send_file("lyra.png")
+
+@app.route('/data', methods=['POST'])
+def post():
+    data = request.get_json(force=True)
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000)
